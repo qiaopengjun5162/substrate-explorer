4dbd7a9421fafe687489d40b284f53e8f96f4f3d
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-12-29 15:46:36 +0800

    update poe and add template project

--- substrate-poe/runtime/src/lib.rs
+++ substrate-poe/runtime/src/lib.rs
@@ -274,6 +274,12 @@ impl pallet_template::Config for Runtime {
 	type WeightInfo = pallet_template::weights::SubstrateWeight<Runtime>;
 }
 
+// Configure the pallet-poe in pallets/poe.
+impl pallet_poe::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type MaxClaimLength = ConstU32<512>;
+}
+
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
 	pub struct Runtime {
@@ -286,6 +292,7 @@ construct_runtime!(
 		Sudo: pallet_sudo,
 		// Include the custom logic from the pallet-template in the runtime.
 		TemplateModule: pallet_template,
+		PoeModule: pallet_poe,
 	}
 );
 
